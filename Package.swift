f0d64cb5d0abc5e30c282f0786a5ebce3a9d9d1b
Antony Perkov <10457437+aperkov@users.noreply.github.com>
2025-03-14 14:40:34 +1100

    Enabling "Swift 6 Language Mode" and "Warnings As Errors"

--- Package.swift
+++ Package.swift
@@ -21,18 +21,28 @@ let package = Package(
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
         .target(
             name: "DecimalMacro",
-            dependencies: ["DecimalMacroImpl"]
+            dependencies: ["DecimalMacroImpl"],
+            swiftSettings: swiftSettings
         ),
         .testTarget(
             name: "DecimalMacroTests",
             dependencies: [
                 "DecimalMacroImpl",
                 .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
-            ]
+            ],
+            swiftSettings: swiftSettings
         ),
     ]
 )
+
+var swiftSettings: [SwiftSetting] {
+    [
+        .swiftLanguageMode(.v6),
+        .unsafeFlags(["-warnings-as-errors"]),
+    ]
+}
